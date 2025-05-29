3907973faa7b6f9b5cf0a747c6faddc5fe120abf
Devashish <devashish@brilliantechsoft.com>
2025-05-29 12:28:10 +0530

    getAllUsers

--- src/repositories/user.repository.js
+++ src/repositories/user.repository.js
@@ -3,3 +3,9 @@ const { User } = require('../models');
 exports.create = async (data) => {
   return await User.create(data);
 };
+
+
+
+exports.getAllUsers = async () => {
+  return await User.findAll();
+};
